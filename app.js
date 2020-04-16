9c76c4fea7337bf49588754392aeb3f73e5cdedd
cflo <cflo013@gmail.com>
2020-04-16 13:25:16 +0800

    correct app.listen

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ const bot = linebot({
 const linebotParser = bot.parser()
 
 bot.on('message', function (event) { // event.message.text是使用者傳給bot的訊息
-  console.log(event)
 
-  let replyMsg = `Hello你剛才說的是:${event.message.text}`
+  var replyMsg = `Hello你剛才說的是:${event.message.text}`
   event.reply(replyMsg).then(function (data) { //將訊息回傳給使用者
     // success 
   }).catch(function (error) {
@@ -30,12 +29,6 @@ app.get('/', (req, res) => {
   res.send(`<H1>Hi LineBOT</h1>`)
 })
 
-// Bot所監聽的webhook路徑與port
-bot.listen(process.env.PORT || 3000, () => {
-  console.log('LineBOT 已經準備好了')
-})
-
-
 app.listen(process.env.PORT || 3000, () => {
-  console.log('APP is running on express')
+  console.log('Express server for LineBOT start')
 })
